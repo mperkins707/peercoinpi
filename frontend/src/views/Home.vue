97fcf9fdeb60f9551e1ce317d6e600b2455ecb4d
Mike Perkins <immike1415@gmail.com>
2022-08-07 18:48:29 -0400

    Update README.md + Add PeercoinRPC

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Balances</h1>
+    {{ this.balances }}
+    <h1>listMinting</h1>
+    {{ this.mintings }}
+    <h1>networkinfo</h1>
+    {{ this.networkinfo }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PeercoinRPC from '@/services/PeercoinRPC.js';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      balances: {
+        type: Array
+      },
+      mintings: {
+        type: Array
+      },
+      networkinfo: {
+        type: Array
+      }
+    }
+  },
+  beforeMount() {
+      let peercoinRpc = new PeercoinRPC();
+      this.balances = peercoinRpc.getBalances();
+      this.mintings = peercoinRpc.getListMintings();
+      this.networkinfo = peercoinRpc.getNetworkInfo();
   }
 }
 </script>
