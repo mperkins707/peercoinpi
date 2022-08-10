0ccb070ef49eecbf9cbc9fcf8f546cbdc136d09c
Mike Perkins <immike1415@gmail.com>
2022-08-10 00:37:40 -0400

    Add dist frontend

---
+++ frontend/dist/precache-manifest.8ce104d61d40b7df8733b53b93321199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd846bf1a6c7485ceb6",
+    "url": "/css/app.61578f0f.css"
+  },
+  {
+    "revision": "7d2d89348ef6cfab2e4717aaf74bb62e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0011858e9b632fa36ad8",
+    "url": "/js/about.5c9ab7c6.js"
+  },
+  {
+    "revision": "7bd846bf1a6c7485ceb6",
+    "url": "/js/app.61e301cd.js"
+  },
+  {
+    "revision": "b6a7343e533e09ba9a5b",
+    "url": "/js/chunk-vendors.440ec915.js"
+  },
+  {
+    "revision": "58de1a6f86dd5dcdb23c7eb90a43084d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
